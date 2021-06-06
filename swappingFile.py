b53d218d0a46db3a82e505b716321e3cd4af4a14
AritroChowdhury <71551202+AritroChowdhury@users.noreply.github.com>
2021-06-06 13:09:13 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,11 @@
+def swapFileData():
+    fileName=input('Enter The File Name-')
+    file=open(fileName,'r')
+    noOfWords=0
+    for line in file:
+        word=line.split()
+        noOfWords=noOfWords+len(word)
+    print('No Of Words' )
+    print(noOfWords)
+
+countWordsFromFile()    
